baaead41f2abde69d600105079a0217fbae18790
Josie Thompson <dot.slash.josie@gmail.com>
2025-08-05 21:57:43 -0700

    quiz revisions - revised quiz questions for hello.cpp - added answer key to hello.cpp - add quiz questions for cmake

--- HelloSDL/hello.cpp
+++ HelloSDL/hello.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <cstdint>
+#include <string>
 
 // We'll just do a few simple things in this example:
 // - boot up SDL
@@ -14,15 +15,10 @@ int main(int argc, char * argv[])
 {
     // These are arbitrary values we'll use to configure our program. Feel free to change them to your liking.
     // We've hard-coded them for the sake of simplicity, but in the future we may want to read them from a file.
-    constexpr char window_name[] = "Hello SDL2";
-    constexpr int window_width = 640;
-    constexpr int window_height = 480;
-    constexpr std::uint32_t program_quit_delay_ms = 2000; // 2000 ms = 2 sec
-
-    // Pop Quiz (Advanced C++):
-    // 1.a) How does the keyword constexpr change how we use these variables?
-    // 1.b) What are some reasons why I might want to declare window_name as char[] instead of string?
-    // 1.c) If we read these values from a file, can they still be constexpr?
+    const std::string window_name = "Hello SDL2";
+    const int window_width = 640;
+    const int window_height = 480;
+    const std::uint32_t program_quit_delay_ms = 2000; // 2000 ms = 2 sec
 
     // For the rest of this file, any time you see a function call that starts with "SDL_",
     // look up the function on https://wiki.libsdl.org and ask yourself these questions:
@@ -35,21 +31,19 @@ int main(int argc, char * argv[])
     }
 
     // Pop Quiz (SDL):
-    // 2.a) Why do we need to initialize SDL?
-    // 2.b) If I wanted to use audio and a controller in addition to video, how would I initialize those?
-    // 2.c) If we hadn't defined the macro SDL_INIT_SUCCESS earlier, the conditional expression would have been
+    // 1.a) If I wanted to use audio and a controller in addition to video, how would I initialize those?
+    // 1.b) If we hadn't defined the macro SDL_INIT_SUCCESS earlier, the conditional expression would have been
     //      SDL_Init(SDL_INIT_VIDEO) != 0. Is it clear what role 0 plays? How might that expression be misread?
 
-    SDL_Window * window = SDL_CreateWindow(window_name,
+    SDL_Window * window = SDL_CreateWindow(window_name.c_str(),
                                            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                                            window_width, window_height,
                                            SDL_WINDOW_SHOWN);
 
     // Pop Quiz (SDL):
-    // 3.a) How does SDL_WINDOWPOS_UNDEFINED affect the position of the window on the screen?
-    // 3.b) How would you make the window appear centered on the screen?
-    // 3.c) Why might I have chosen to put SDL_WINDOW_SHOWN in the last argument - does it need to be there?
-    //      If you add other window flags, why would you keep or remove SDL_WINDOW_SHOWN?
+    // 2.a) How would you make the window appear centered on the screen?
+    // 2.b) Why might I have chosen to put SDL_WINDOW_SHOWN in the last argument - does it need to be there?
+    // 2.c) If you add other window flags, why would you keep or remove SDL_WINDOW_SHOWN?
 
     if (window == nullptr) {
         SDL_Log("Couldn't create a window in SDL2: %s\n", SDL_GetError());
@@ -62,8 +56,22 @@ int main(int argc, char * argv[])
     SDL_Quit();
 
     // Pop Quiz (SDL and C++):
-    // 4.a) Why do we need to call SDL_Quit() before we return EXIT_FAILURE if the window wasn't created?
-    // 4.b) What happens if we remove the calls to SDL_DestroyWindow and SDL_Quit?
+    // 3.a) Why do we need to call SDL_Quit() before we return EXIT_FAILURE if the window wasn't created?
+    // 3.b) What happens if we remove the calls to SDL_DestroyWindow and SDL_Quit?
 
     return EXIT_SUCCESS;
 }
+
+// Answer Key:
+// 1.a) SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMEPAD)
+// 1.b) 0 is often used as a boolean value for false: you might incorrectly assume 0 means SDL didn't initialize
+//
+// 2.a) Use SDL_WINDOWPOS_CENTERED
+// 2.b) SDL_WINDOW_SHOWN isn't needed, and is in fact ignored unless SDL_WINDOW_HIDDEN was previously set.
+//      I add it here because it's unclear what the last argument does if you just pass a zero.
+// 2.c) You may want to remove SDL_WINDOW_SHOWN if you pass other window flags because it's ignored.
+//      However, you may want to keep the flag just to be explicit/descriptive.
+//
+// 3.a) SDL allocates resources when its subsystems are initialized.
+//      These resources always need to be deallocated to prevent a memory leak.
+// 3.b) Memory leak!
